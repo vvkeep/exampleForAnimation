f4355ee7778b69856adf080b70002722cec35053
姚巍 <15919688564@163.com>
2016-12-18 10:14:28 +0800

    Initial Commit

---
+++ exampleForAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exampleForAnimation
+//
+//  Created by 姚巍 on 16/12/18.
+//  Copyright © 2016年 姚巍. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
